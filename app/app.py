d7675951caca6415b6f8d43bc39b36f678096efd
Shamik Ray <shamik.ray@coviam.com>
2017-10-29 15:48:43 +0530

    New changes

--- app/app.py
+++ app/app.py
@@ -6,151 +6,215 @@ from flask.ext.cache import Cache
 from random import randint
 
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-	return "Yo, it's working!"
+    return "Yo, it's working!"
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
 
-WORDNIK_URL='http://api.wordnik.com:80/v4/'
+WORDNIK_URL = 'http://api.wordnik.com:80/v4/'
 API_KEY = '6b7418187bd740d53c01975443c56826e52ec538526c7875f'
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
-@cache.memoize(timeout=3600)
+@cache.memoize(timeout=86400)
 def get_auth_token():
-	params={
-		'api_key' : API_KEY,
-		'password':'jacksparrow'
-	}
+    params = {
+        'api_key': API_KEY,
+        'password': 'jacksparrow'
+    }
 
-	response = requests.get(WORDNIK_URL+'account.json/authenticate/raynaya',params=params)
-	print(response.text)
-	if response.status_code == 200:
-		response = response.json()
-		return response.get('token')
-	# else:
-	# 	return json.dumps({'msg': 'Failed to get auth token'})
+    response = requests.get(
+        WORDNIK_URL + 'account.json/authenticate/raynaya', params=params)
+    print(response.text)
+    if response.status_code == 200:
+        response = response.json()
+        return response.get('token')
+    # else:
+    # 	return json.dumps({'msg': 'Failed to get auth token'})
+
 
 @cache.memoize(timeout=86400)
-def get_word_list(url,params,headers):
-	response = requests.get(url, params=params, headers=headers)
-	print(response.text)
-	if response.status_code != 200:
-		return None
+def get_word_list(url, params, headers):
+    response = requests.get(url, params=params, headers=headers)
+    print(response.text)
+    if response.status_code != 200:
+        return None
 
-	return response.json()
+    return response.json()
+
+
+@cache.memoize(timeout=604800)
+@app.route('/get_usage/<word>', methods=['GET'])
+def get_usage(word):
+
+    params = {
+        'useCanonical': False,
+        'api_key': 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+    }
+    try:
+        print(WORDNIK_URL + 'v4/word.json/{}/topExample'.format(word))
+        response = requests.get(
+            'http://api.wordnik.com/v4/word.json/{}/topExample'.format(word), params=params)
+        print(response.text)
+        if response.status_code == 200:
+            response = response.json()
+            title = response.get('word')
+            title = title.title()
+            print(title)
+            if not title:
+                return json.dumps({'msg': 'Failed to get a word'})
+
+            usage = response.get('text')
+            if not usage:
+                return json.dumps({'msg': 'Failed to get usage examples'})
+
+            url = 'https://www.wordnik.com/words/{}'.format(word)
+
+            payload = {
+                'data': {
+                    'type': 'carousel',
+                    'templates': [{
+                            'title': 'Word : {}\n{}'.format(title, usage),
+
+                        'default_action': {
+                                'type': 'web_url',
+                                'url': url
+                                },
+                        'buttons': [
+                                {
+                                    'title': 'Related Words',
+                                    'type': 'postback',
+                                    'payload': ''
+                                }
+                            ]
+                    }
+                    ]
+                }
+            }
+            return json.dumps(payload)
+        else:
+            return json.dumps({'msg': 'Failed to get get_word_examples'})
+
+    except Exception:
+        return json.dumps({'msg': 'Failed to get examples'})
+
 
 @app.route('/random_word/', methods=['GET'])
 def random_word():
-	auth_token = get_auth_token()
-	if not auth_token:
-		return json.dumps({'msg': 'Failed to get auth token'})
-	try:
-		params={
-			'api_key': API_KEY
-		}
-		headers = {
-			'auth_token':auth_token
-		}
-		wordList = get_word_list(WORDNIK_URL+'wordList.json/wordbot/words', params, headers)
+    auth_token = get_auth_token()
+    if not auth_token:
+        cache.delete_memoized(get_auth_token)
+        return json.dumps({'msg': 'Failed to get auth token'})
+    try:
+        params = {
+            'api_key': API_KEY
+        }
+        headers = {
+            'auth_token': auth_token
+        }
+        wordList = get_word_list(
+            WORDNIK_URL + 'wordList.json/wordbot/words', params, headers)
 
-		if not wordList:
-			return json.dumps({'msg': 'Failed to get wordList'})
-		
-		random_index = randint(0, len(wordList))
-		word = wordList[random_index].get('word')
+        if not wordList:
+            return json.dumps({'msg': 'Failed to get wordList'})
 
-		params={
-			'limit':1,
-			'includeRelated' : True,
-			'sourceDictionaries':'wiktionary',
-			'useCanonical' : False,
-			'includeTags' : False,
-			'api_key': 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
-		}
+        random_index = randint(0, len(wordList))
+        word = wordList[random_index].get('word')
 
-		response = requests.get(WORDNIK_URL+'word.json/{}/definitions'.format(word), params=params, headers=headers)
-		if response.status_code != 200:
-			return json.dumps({'msg': 'Failed to get wordDefinition'})
+        params = {
+            'limit': 1,
+            'includeRelated': True,
+            'sourceDictionaries': 'wiktionary',
+            'useCanonical': False,
+            'includeTags': False,
+            'api_key': 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+        }
 
-		response=response.json()
-		meaning = response[0].get('text')
-		pos = response[0].get('partOfSpeech')
+        response = requests.get(
+            WORDNIK_URL + 'word.json/{}/definitions'.format(word), params=params, headers=headers)
+        if response.status_code != 200:
+            return json.dumps({'msg': 'Failed to get wordDefinition'})
 
-		url = 'https://www.wordnik.com/words/{}'.format(word)
+        response = response.json()
+        meaning = response[0].get('text')
+        pos = response[0].get('partOfSpeech')
 
-		payload  = {
-			'data' : {
-				'type':'carousel',
-				 'templates' : [{
-					'title' : 'Word Of the day : {}'.format(word),
-					'subtitle': 'Definition\n{}'.format(meaning),
-					'default_action' : {
-						'type' : 'web_url',
-						'url' : url
-						}
-					}
-				]
-			}
-		}
-		return json.dumps(payload)
-	
-	except Exception:
-		return json.dumps({'msg': 'Failed to get a word'})
+        url = 'https://www.wordnik.com/words/{}'.format(word)
+
+        payload = {
+            'data': {
+                'type': 'carousel',
+                'templates': [{
+                        'title': '{}\nDefinition\n{}'.format(word, meaning),
+                    'default_action': {
+                            'type': 'web_url',
+                            'url': url
+                    }
+                }
+                ]
+            }
+        }
+        return json.dumps(payload)
+
+    except Exception:
+        return json.dumps({'msg': 'Failed to get a word'})
+
 
 @app.route('/word_of_the_day/', methods=['GET'])
-@cache.memoize(timeout=3600)
+@cache.memoize(timeout=86400)
 def word_of_the_day():
-	#http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=2017-10-15&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
-	params={
-		'date' : datetime.datetime.today().strftime('%Y-%m-%d'),
-		'api_key' : 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
-	}
-	try:
-		response = requests.get(WORDNIK_URL+'words.json/wordOfTheDay', params=params)
-		print(response.text)
-		if response.status_code == 200:
-			response = response.json()
-			title = response.get('word')
-			title = title.title()
-			print(title)
-			if not title:
-				return json.dumps({'msg': 'Failed to get a word'})
+    # http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=2017-10-15&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
+    params = {
+        'date': datetime.datetime.today().strftime('%Y-%m-%d'),
+        'api_key': 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+    }
+    try:
+        response = requests.get(
+            WORDNIK_URL + 'words.json/wordOfTheDay', params=params)
+        print(response.text)
+        if response.status_code == 200:
+            response = response.json()
+            title = response.get('word')
+            title = title.title()
+            print(title)
+            if not title:
+                return json.dumps({'msg': 'Failed to get a word'})
 
-			definitions = response.get('definitions')
-			if definitions:
-				meaning = definitions[0].get('text')
-				if not meaning:
-					return json.dumps({'msg': 'Failed to get a meaning'})
-			else:
-				return json.dumps({'msg': 'Failed to get a definition'})
-			print(meaning)
+            definitions = response.get('definitions')
+            if definitions:
+                meaning = definitions[0].get('text')
+                if not meaning:
+                    return json.dumps({'msg': 'Failed to get a meaning'})
+            else:
+                return json.dumps({'msg': 'Failed to get a definition'})
+            print(meaning)
 
-			note = response.get('note','')
-			url = 'https://www.wordnik.com/word-of-the-day'
+            note = response.get('note', '')
+            url = 'https://www.wordnik.com/word-of-the-day'
 
-			payload  = {
-				'data' : {
-					'type':'carousel',
-					 'templates' : [{
-						'title' : 'Word Of the day : {}'.format(title),
-						'subtitle': 'Definition\n{}'.format(meaning),
-						'default_action' : {
-							'type' : 'web_url',
-							'url' : url
-							}
-						}
-					]
-				}
-			}
-			return json.dumps(payload)
-		else:
-			return json.dumps({'msg': 'Failed to get a word'})
-	except Exception:
-		return json.dumps({'msg': 'Failed to get a word'}),500
+            payload = {
+                'data': {
+                    'type': 'carousel',
+                    'templates': [{
+                            'title': 'Word Of the day : {}'.format(title),
+                        'subtitle': 'Definition\n{}'.format(meaning),
+                                    'default_action': {
+                                'type': 'web_url',
+                                'url': url
+                                }
+                    }
+                    ]
+                }
+            }
+            return json.dumps(payload)
+        else:
+            return json.dumps({'msg': 'Failed to get a word'})
+    except Exception:
+        return json.dumps({'msg': 'Failed to get a word'}), 500
